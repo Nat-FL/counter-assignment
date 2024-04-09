e1a6301110b02f18d70aa0630a6b855bf2b4bc4f
Nat-FL <nrgraddy@gmail.com>
2024-04-09 14:28:53 -0400

    updated Counter.test with properly functioning tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,15 +1,22 @@
+// import necessary react testing library helpers here
+// import the Counter component here
 import { render, screen, fireEvent} from '@testing-library/react';
 import Counter from "../components/Counter";
 
-test('renders counter message', () => {
+// beforeEach(() => {
+//   // Render the Counter component here
+//   render(<Counter/>);
+// })
 
+test('renders counter message', () => {
   render(<Counter />);
+
   const counterMessage = screen.getByText(/Counter/i);
   expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-
+ 
   const {getByTestId} = render(<Counter initialCount={0} />);
   const countVal = getByTestId("count"); 
   expect(countVal.textContent).toBe('0');
@@ -25,7 +32,7 @@ test('clicking + increments the count', () => {
 });
 
 test('clicking - decrements the count', () => {
- 
+
   const {getByTestId, getByRole} = render(<Counter initialCount={0} />);
   const incrementBtn = getByRole("button", {name:"-"});
   fireEvent.click(incrementBtn);
